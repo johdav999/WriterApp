6a07310a1ee1b087ab1948fb00be7b7953e97e19
Johan Davidsson <johandavidsson@hotmail.se>
2026-01-11 18:29:15 +0100

    Implemented load document

--- obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b26ab7b87cb7acb02a114006b34356638b5247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a0bdef45793144c224415bf93b7134e27dbef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
