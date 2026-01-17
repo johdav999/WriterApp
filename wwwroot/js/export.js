a20ae0837b5fd51a42780cb6ad9cff9c099526f9
Johan Davidsson <johandavidsson@hotmail.se>
2026-01-17 12:00:10 +0100

    Fixed rebase errors

--- wwwroot/js/export.js
+++ wwwroot/js/export.js
@@ -21,3 +21,27 @@ export function downloadFile(base64Data, mimeType, fileName) {
     URL.revokeObjectURL(url);
 }
 
+export function printHtmlAsPdf(html) {
+    const frame = document.createElement("iframe");
+    frame.style.position = "fixed";
+    frame.style.right = "0";
+    frame.style.bottom = "0";
+    frame.style.width = "0";
+    frame.style.height = "0";
+    frame.style.border = "0";
+
+    document.body.appendChild(frame);
+
+    const doc = frame.contentDocument || frame.contentWindow.document;
+    doc.open();
+    doc.write(html);
+    doc.close();
+
+    frame.contentWindow.focus();
+    frame.contentWindow.print();
+
+    setTimeout(() => {
+        document.body.removeChild(frame);
+    }, 1000);
+}
+
